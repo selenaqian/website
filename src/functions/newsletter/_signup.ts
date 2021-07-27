9b2ec40e935d4b897973cb143dffcf59a33ec659
nisarhassan12 <syednisarhassan12@gmail.com>
2021-07-27 15:29:56 +0500

    pass only the valid arguments.

--- src/functions/newsletter/_signup.ts
+++ src/functions/newsletter/_signup.ts
@@ -1,11 +1,11 @@
 import Analytics from 'analytics-node';
 import type { Response } from "@netlify/functions/src/function/response";
-import saveEmailInSheet from "../feedback/_save-to-spreadsheet";
 
 const analytics = new Analytics('14WvAwP23g2AZddYGxblnp1BbVAErnsy');
 
 export const signup = async (body: string): Promise<Response> => {
-  analytics.track("newsletter signup", {
+  analytics.track({
+    event: 'Signed Up',
     email: body,
     signup_at: new Date().toISOString(),
   });
