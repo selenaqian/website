c8a0187fdf71f9ffa3450c2435f77758a2b7363c
nisarhassan12 <syednisarhassan12@gmail.com>
2021-07-27 15:40:09 +0500

    pass the require anonymous id.

--- src/functions/newsletter/_signup.ts
+++ src/functions/newsletter/_signup.ts
@@ -1,10 +1,12 @@
 import Analytics from 'analytics-node';
 import type { Response } from "@netlify/functions/src/function/response";
+import { nanoid } from 'nanoid';
 
 const analytics = new Analytics('14WvAwP23g2AZddYGxblnp1BbVAErnsy');
 
 export const signup = async (body: string): Promise<Response> => {
   analytics.track({
+    anonymousId: nanoid(),
     event: 'Signed Up',
     email: body,
     signup_at: new Date().toISOString(),
